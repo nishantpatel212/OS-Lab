420315a7fbf3d4e1b108b6cdfbac741c6514c045
nishantpatel212 <nishantpatel212@gmail.com>
2015-01-24 12:37:16 +0530

    OS Lab 2 Submission
    Shell Script of commands

---
+++ 121031_Lab2.sh
@@ -0,0 +1,36 @@
+cd
+cd Desktop
+factor 45
+spell hi.txt
+seq 2 5 20
+uname -n
+#sudo shutdown -r 1
+diff hi.txt hi1.txt
+df
+du
+du -h
+tty
+who -H
+#aspell -a
+whereis
+lpinfo
+lpinfo -v
+lpinfo -m
+iw
+rev hi1.txt
+last
+sort hi1.txt
+sync
+ifconfig
+who -b
+who -d
+uname -a
+free
+#init
+sdiff hi.txt hi1.txt
+#bzip2 hi.txt
+#head
+#top
+dmesg
+jobs
+yes Thank You
